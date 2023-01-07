e78dd30975cd838f590f427a72ff0cc5ad7e34d5
ruckeb <ruckeb@outlook.es>
2023-01-07 17:35:35 +0100

    enviarMatch mal

---
+++ back/controladores/enviarMatch.php
@@ -0,0 +1,8 @@
+<?php
+    // require_once '../basesDeDatos/bdCuore.php';
+    // $usuario = actualizarMatchBBDD();
+    $usuario = array(
+        'amor' => true 
+    );
+    $json = json_encode($usuario);
+    echo $json;
